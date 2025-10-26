04b452b983587c0e737b349319185e9cede86385
Chase Pasion <chasepasion@gmail.com>
2025-10-25 21:17:08 -1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}
+    >
+    <h1
+    style={{
+      color: "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)",
+          fontSize: "5rem",
+          fontFamily: "Arial, sans-serif",
+    }}
+    >
+    RAINBOW ROAD
+    </h1>
+    </div>
+  );
+}
+
+export default App;
+
